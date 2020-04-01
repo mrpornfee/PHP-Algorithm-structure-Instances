2a1dd403c3d5650ea84cb167a1f5f559f3def1a5
马鹏飞 <937096063@qq.com>
2020-04-01 21:59:18 +0800

    asd

--- test.php
+++ test.php
@@ -9,16 +9,16 @@ function test_autoload($ClassName){
 }
 spl_autoload_register('test_autoload',true,true);
 //↓↓↓↓操作区↓↓↓↓
-for($i=0;$i<8;$i++){
+for($i=0;$i<5000;$i++){
     $arr[$i]=rand(0,999999);
 }
-$button=true;
+$button=false;
 //↑↑↑↑操作区↑↑↑↑
 /*$test1=new BubbleSort($arr);
 $test1->run($button);
 $test2=new SelectionSort($arr);
-$test2->run($button);
-$test2=new InsertSort($arr);
 $test2->run($button);*/
+$test2=new InsertSort($arr);
+$test2->run($button);
 $test2=new ShellSort($arr);
 $test2->run($button);
