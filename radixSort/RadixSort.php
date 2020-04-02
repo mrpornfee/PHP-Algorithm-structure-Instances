25f98e24d8880481d4e49bbb55ac75b2b2cee1b0
sulifer <937096063@qq.com>
2020-04-02 16:13:20 +0800

    'asdasd'

---
+++ radixSort/RadixSort.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 93709
+ * Date: 2020/4/2
+ * Time: 14:50
+ */
+namespace radixSort;
+class RadixSort
+{
+    use \tools\microtimeToFloat;
+
+    private static  $arr;
+
+    public function __construct($arr)
+    {
+        if(is_array($arr)) self::$arr=$arr;
+        else {
+            echo '需用数组初始化';
+            exit;
+        }
+    }
+
+    private function sort($show){
+        $bucket=array_fill(0,10,null);
+        $max=max(self::$arr);
+        for($i=1;$i<=$max;$i*=10) {
+            for ($j = 0; $j < sizeof(self::$arr); $j++) {
+                $digit = ((int)(self::$arr[$j] / $i)) % 10;
+                $bucket[$digit][] = self::$arr[$j];
+            }
+            for ($j = 0; $j < 10; $j++) {
+                while ($bucket[$j] != null) {
+                    $tmp[]=$bucket[$j][0];
+                     array_shift($bucket[$j]);
+                }
+            }
+            self::$arr = $tmp;
+            $tmp=[];
+            $this->show($show);
+        }
+    }
+
+    private function show($show=true){
+        if($show==true) {
+            for ($i = 0; $i < sizeof(self::$arr); $i++) {
+                echo self::$arr[$i] . '  ';
+            }
+            echo PHP_EOL;
+            echo PHP_EOL;
+        }
+    }
+
+    public function run($show=true){
+        $this->show($show);
+        $timeBefore=$this->getTime();
+        $this->sort($show);
+        $timeAfter=$this->getTime();
+        echo '基数排序执行时间为'.($timeAfter-$timeBefore).'秒'.PHP_EOL;
+    }
+
+}
--- test.php
+++ test.php
@@ -6,12 +6,13 @@ use insertSort\InsertSort;
 use shellSort\ShellSort;
 use quickSort\QuickSort;
 use mergeSort\MergeSort;
+use radixSort\RadixSort;
 function test_autoload($ClassName){
     require_once __DIR__.DIRECTORY_SEPARATOR .$ClassName.".php";
 }
 spl_autoload_register('test_autoload',true,true);
 //↓↓↓↓操作区↓↓↓↓
-for($i=0;$i<5000;$i++){
+for($i=0;$i<100;$i++){
     $arr[$i]=rand(0,9999);
 }
 $button=0;
@@ -23,7 +24,7 @@ $instance->main();
 $method=$class->getMethod('main');
 $method->invoke($instance);*/
 
-/*
+
 $test1=new BubbleSort($arr);
 $test1->run($button);
 $test2=new SelectionSort($arr);
@@ -33,6 +34,8 @@ $test3->run($button);
 $test4=new ShellSort($arr);
 $test4->run($button);
 $test5=new QuickSort($arr);
-$test5->run($button);*/
+$test5->run($button);
 $test5=new MergeSort($arr);
 $test5->run($button);
+$test6=new RadixSort($arr);
+$test6->run($button);
