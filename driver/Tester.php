64451b6df281a7e5cd2821b042ebee85ff86f156
sulifer <937096063@qq.com>
2020-04-03 10:05:16 +0800

    driver

---
+++ driver/Tester.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 93709
+ * Date: 2020/4/2
+ * Time: 16:36
+ */
+namespace driver;
+class Tester
+{
+    private static $classArr;
+
+    private static $path=DIRECTORY_SEPARATOR.'algorithm'.DIRECTORY_SEPARATOR;
+
+    private function __construct()
+    {
+    }
+
+    /**绑定对象实列
+     * @param $class  //数组或字符串，algorithm下的相对路径
+     * @param $arr
+     * @return bool
+     */
+    public static function bind($class,$arr)
+    {
+        if (is_string($class)) {
+                $class=self::$path.$class;
+            if (self::$classArr == null || !array_key_exists($class, self::$classArr)) {
+                self::$classArr[$class] = new $class($arr);
+            }
+            return true;
+        } else if (is_array($class)) {
+            for ($i = 0; $i < sizeof($class); $i++) {
+                $class[$i]=self::$path.$class[$i];
+                if (self::$classArr == null || !array_key_exists($class[$i], self::$classArr)) {
+                    self::$classArr[$class[$i]] = new $class[$i]($arr);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**卸载对象实例
+     * @param $class //数组或字符串，algorithm下的相对路径
+     * @return bool
+     */
+    public static function unload($class)
+    {
+        if (is_string($class)) {
+            $class=self::$path.$class;
+            if (self::$classArr != null && array_key_exists($class, self::$classArr)) {
+                unset(self::$classArr[$class]);
+                return true;
+            }
+            return false;
+        } else if (is_array($class)) {
+            $t = sizeof($class);
+            for ($i = 0; $i < $t; $i++) {
+                $class[$i]=self::$path.$class[$i];
+                if (self::$classArr == null || !array_key_exists($class[$i], self::$classArr))
+                    return false;
+            }
+            for ($i = 0; $i < $t; $i++) {
+                unset(self::$classArr[$class[$i]]);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**运行对象实列
+     * 只运行run()方法
+     * @param $class //数组或字符串，algorithm下的相对路径
+     * @param $show  //是否显示过程 1或者true 显示 0或false 不显示
+     * @return bool
+     */
+    public static function run($class,$show){
+        if(self::$classArr==null) return false;
+        if(is_string($class)){
+            $class=self::$path.$class;
+            if(array_key_exists($class,self::$classArr)){
+                self::$classArr[$class]->run($show);
+                return true;
+            }
+            return false;
+        }
+        if(is_array($class)){
+            $t=sizeof($class);
+            for($i=0;$i<$t;$i++){
+                $class[$i]=self::$path.$class[$i];
+                if(!array_key_exists($class[$i],self::$classArr))
+                    return false;
+            }
+            for($i=0;$i<$t;$i++){
+                self::$classArr[$class[$i]]->run($show);
+            }
+            return true;
+        }
+        return false;
+    }
+}
